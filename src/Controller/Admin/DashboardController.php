d1c09b610333f4873140963c9b11f425a0cbf2b0
danicolasss <danny-nicolas.grangeot@hotmail.com>
2023-01-04 11:32:08 +0100

    installation de easy admin et du lien pour acceder au back office

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+
+    public function __construct( private AdminUrlGenerator $adminUrlGenerator)
+    {
+
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator->setController(EtablissementCrudController::class)->generateUrl();
+        return  $this->redirect($url);
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Symfony Project');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
